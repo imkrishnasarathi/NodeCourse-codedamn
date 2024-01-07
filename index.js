94e67c6ba7c511d3418f6e1f00fa5f3696ab3f8d
Krishna Sarathi Ghosh <krishnasarathishem@gmail.com>
2024-01-07 19:43:17 +0530

    update

--- index.js
+++ index.js
@@ -4,9 +4,10 @@ const { createFile } = require('./util.js');
 const fs = require('fs');
 
 // Use as promise 
-const _fs = require('fs');
-const fs = _fs.promises;
+/* const _fs = require('fs');
+const fs = _fs.promises; */
 
 console.log(fs.readdirSync('.'));
+console.log(fs.readFileSync('index.js', 'utf8'));
 
 // createFile("test.txt", "Hello World!");
