969b64c607330d56a35b27d57833657c896ed1e2
Krishna Sarathi Ghosh <krishnasarathishem@gmail.com>
2024-01-07 19:51:47 +0530

    es6.mjs

--- modules.mjs
+++ modules.mjs
@@ -2,6 +2,14 @@ function square(num){
     return num**2;
 }
 
+function cube(num){
+    return num**3;
+}
+
 // module.exports = { square };
 
-export default square;
+
+export {
+    square,
+    cube
+}
