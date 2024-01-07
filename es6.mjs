184dc558048bfcbad8021b39ff939aa3fb1077c0
Krishna Sarathi Ghosh <krishnasarathishem@gmail.com>
2024-01-07 19:49:46 +0530

    es6.mjs

--- es6.mjs
+++ es6.mjs
@@ -1,5 +1,9 @@
-import fs from 'fs';
+// import fs from 'fs';
 
-console.log(
-    fs.readFileSync("common.js", "utf8")
-)
+// console.log(
+//     fs.readFileSync("common.js", "utf8")
+// )
+
+import square from "./modules"
+
+console.log(square(5));
