974def9b53367268cf9663c03d609ce8d1873b88
Krishna Sarathi Ghosh <krishnasarathishem@gmail.com>
2024-01-07 19:38:56 +0530

    update

--- util.js
+++ util.js
@@ -9,9 +9,17 @@
 
 // module.exports.addNumbers = addNumbers;
 
+// http request:
+
+// 1. Async http
+
+// 2. Sync httpSync
+
 const fs = require('fs');
 
 function createFile(name, contents){
     fs.writeFileSync(name, contents);
     console.log(`File ${name} created`);
 }
+
+module.exports.createFile = createFile;
